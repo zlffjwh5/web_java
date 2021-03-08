d00aceadb730c63d5ebf30e87551beab1d5d25bb
OHchan <42002548+zlffjwh5@users.noreply.github.com>
2021-03-08 16:48:43 +0900

    add sub

--- Day_02_Variable_O/src/com/kh/Case_01.java
+++ Day_02_Variable_O/src/com/kh/Case_01.java
@@ -3,5 +3,8 @@ package com.kh;
 public class Case_01 {
 	public static void main(String[] args) {
 		System.out.println("Hello java!");
+		//hello world~
+		// every code learning started 'hello wrold' but this is java course 
+		//so I'd started with 'hello java' :)
 	}
 }
